ba1fbbc807a917056b90e6badaf0938c04a14f41
willsllc <willsllc@codeplex.com>
2008-01-17 03:04:09 +0100

    Initial checkin

---
+++ VCardSplitter/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace VCardSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbSource = new System.Windows.Forms.GroupBox();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.gbTarget = new System.Windows.Forms.GroupBox();
+            this.btnSelectTarget = new System.Windows.Forms.Button();
+            this.tbTarget = new System.Windows.Forms.TextBox();
+            this.fbdTarget = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.ofdSource = new System.Windows.Forms.OpenFileDialog();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.gbSource.SuspendLayout();
+            this.gbTarget.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSource
+            // 
+            this.gbSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbSource.Controls.Add(this.btnSelectSource);
+            this.gbSource.Controls.Add(this.tbSource);
+            this.gbSource.Location = new System.Drawing.Point(12, 12);
+            this.gbSource.Name = "gbSource";
+            this.gbSource.Size = new System.Drawing.Size(289, 49);
+            this.gbSource.TabIndex = 0;
+            this.gbSource.TabStop = false;
+            this.gbSource.Text = "Source File";
+            // 
+            // tbSource
+            // 
+            this.tbSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSource.Location = new System.Drawing.Point(6, 19);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(241, 20);
+            this.tbSource.TabIndex = 0;
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectSource.Location = new System.Drawing.Point(253, 19);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(30, 20);
+            this.btnSelectSource.TabIndex = 1;
+            this.btnSelectSource.Text = "...";
+            this.btnSelectSource.UseVisualStyleBackColor = true;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // gbTarget
+            // 
+            this.gbTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbTarget.Controls.Add(this.btnSelectTarget);
+            this.gbTarget.Controls.Add(this.tbTarget);
+            this.gbTarget.Location = new System.Drawing.Point(12, 67);
+            this.gbTarget.Name = "gbTarget";
+            this.gbTarget.Size = new System.Drawing.Size(289, 49);
+            this.gbTarget.TabIndex = 2;
+            this.gbTarget.TabStop = false;
+            this.gbTarget.Text = "Target Directory";
+            // 
+            // btnSelectTarget
+            // 
+            this.btnSelectTarget.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectTarget.Location = new System.Drawing.Point(253, 19);
+            this.btnSelectTarget.Name = "btnSelectTarget";
+            this.btnSelectTarget.Size = new System.Drawing.Size(30, 20);
+            this.btnSelectTarget.TabIndex = 1;
+            this.btnSelectTarget.Text = "...";
+            this.btnSelectTarget.UseVisualStyleBackColor = true;
+            this.btnSelectTarget.Click += new System.EventHandler(this.btnSelectTarget_Click);
+            // 
+            // tbTarget
+            // 
+            this.tbTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTarget.Location = new System.Drawing.Point(6, 19);
+            this.tbTarget.Name = "tbTarget";
+            this.tbTarget.Size = new System.Drawing.Size(241, 20);
+            this.tbTarget.TabIndex = 0;
+            // 
+            // fbdTarget
+            // 
+            this.fbdTarget.Description = "Select a folder to save to...";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(12, 122);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(71, 23);
+            this.btnSplit.TabIndex = 3;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // ofdSource
+            // 
+            this.ofdSource.DefaultExt = "vcf";
+            this.ofdSource.Filter = "VCards|*.vcf";
+            this.ofdSource.SupportMultiDottedExtensions = true;
+            this.ofdSource.Title = "Select a VCard file to split...";
+            this.ofdSource.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdSource_FileOk);
+            // 
+            // progress
+            // 
+            this.progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progress.Location = new System.Drawing.Point(89, 122);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(212, 23);
+            this.progress.Step = 1;
+            this.progress.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 155);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.gbTarget);
+            this.Controls.Add(this.gbSource);
+            this.Name = "Form1";
+            this.Text = "VCard Splitter";
+            this.gbSource.ResumeLayout(false);
+            this.gbSource.PerformLayout();
+            this.gbTarget.ResumeLayout(false);
+            this.gbTarget.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSource;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.GroupBox gbTarget;
+        private System.Windows.Forms.Button btnSelectTarget;
+        private System.Windows.Forms.TextBox tbTarget;
+        private System.Windows.Forms.FolderBrowserDialog fbdTarget;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.OpenFileDialog ofdSource;
+        private System.Windows.Forms.ProgressBar progress;
+    }
+}
+
---
+++ VCardSplitter/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VCardSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ofdSource_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnSelectSource_Click(object sender, EventArgs e)
+        {
+            if (this.ofdSource.ShowDialog() == DialogResult.OK)
+            {
+                this.tbSource.Text = this.ofdSource.FileName;
+            }
+        }
+
+        private void btnSelectTarget_Click(object sender, EventArgs e)
+        {
+            if (this.fbdTarget.ShowDialog() == DialogResult.OK)
+            {
+                this.tbTarget.Text = this.fbdTarget.SelectedPath;
+            }
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            String source = this.tbSource.Text;
+            String target = this.tbTarget.Text;
+
+            if (!File.Exists(source)) return;
+            if (!Directory.Exists(target)) return;
+
+            String[] lines = File.ReadAllLines(source);
+            String contents = String.Empty;
+            String name = String.Empty;
+            Boolean isBegin = false;
+            Boolean isEnd = false;
+            Int32 iFiles = 0;
+            this.progress.Maximum = lines.Length;
+            foreach (String line in lines)
+            {
+                if (line == "BEGIN:VCARD") isBegin = true;
+                if (line == "END:VCARD") isEnd = true;
+                if (line.StartsWith("FN:")) name = line.Substring(3) + ".vcf";
+                if (isBegin) contents += System.Environment.NewLine + line;
+                if (isEnd)
+                {
+                    File.WriteAllText(Path.Combine(target, name), contents);
+                    isBegin = false;
+                    isEnd = false;
+                    contents = String.Empty;
+                    iFiles++;
+                }
+                this.progress.PerformStep();
+            }
+
+            MessageBox.Show("Your VCard was split into " + iFiles.ToString() + " files.", "Success", MessageBoxButtons.OK);
+        }
+    }
+}
